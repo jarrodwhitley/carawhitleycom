b8c30357385b3dc394c837fe61a3c1a9ad4a23d7
Jarrod Whitley <jarrodwhitley@gmail.com>
2024-07-23 10:40:31 -0500

    lots of updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html}, ./src/assets/js/*.{js}"],
+  content: ["./*.html", "./assets/js/*.js"],
   theme: {
     extend: {},
   },
